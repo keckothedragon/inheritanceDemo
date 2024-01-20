32183bb3e781fbc8371d64d0a5b92851822cc01e
keckothedragon <flappystar05@gmail.com>
2024-01-19 19:18:13 -0500

    Refactored myShape to be Rectangle

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Shape myShape = new Circle(5);
+        Shape myShape = new Rectangle(5, 10);
         System.out.println(myShape.calculateArea());
         System.out.println(myShape.calculatePerimeter());
     }
